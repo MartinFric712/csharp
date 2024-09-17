c13ee9299a273b2a546827e858ecf3c8911759f0
fricm24 <fricm24@Z00T1-08>
2024-09-17 09:40:28 +0200

    Dokončené cvičenie 008-ConsoleReadLine 17.9 2024

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -1,8 +1,13 @@
 ﻿Console.WriteLine("Ako sa voláš?");
 string? meno = Console.ReadLine();
-Console.WriteLine("Ahoj, " + meno);
+Console.WriteLine("Ahoj, " + meno + "\n");
 
-Console.WriteLine("Koľko máš rokov?");
+Console.WriteLine("Koľko máš rokov? " + meno);
 string vstup = Console.ReadLine() ?? "0";
 int vek = int.Parse(vstup);
-Console.WriteLine("Máš " + vek + " rokov.");
+Console.WriteLine("Máš " + vek + " rokov." + "\n");
+
+Console.WriteLine("Aké je tvoje obľúbené jedlo?");
+string? jedlo = Console.ReadLine();
+Console.WriteLine($"Hmm,  {jedlo}  znie skvele !" + "\n");
+Console.WriteLine($"Volám sa {meno}, mám {vek} rokov a moje obľúbené jedlo je {jedlo}.");
