f64519ea60c1892d1de2d2e7673a6b5be4a8440d
fricm24 <fricm24@Z00T1-08>
2024-09-24 10:18:00 +0200

    Dokončené cvičenie 015_Arrays.ArrayProcesor - 24.9 2024

--- src/AppsLab-015-Arrays/ArrayProcessor.cs
+++ src/AppsLab-015-Arrays/ArrayProcessor.cs
@@ -1,4 +1,6 @@
-﻿namespace AppsLab_015_Arrays;
+﻿using System.Reflection.Metadata;
+
+namespace AppsLab_015_Arrays;
 
 /// <summary>
 /// Class ArrayProcessor provides methods for working with integer arrays.
@@ -13,18 +15,25 @@ public class ArrayProcessor
     /// <returns>First element of the array, or 0 if array is empty.</returns>
     public int GetFirstElement(int[] numbers)
     {
-        throw new NotImplementedException();
+        if (numbers.Length == 0)
+        {
+            return 0;
+        }
+        return numbers[0];
     }
-
     /// <summary>
     /// Returns the last element of the provided array.
     /// If the array is empty, it returns 0.
     /// </summary>
-    /// <param name="numbers">Array of integers.</param>
+    /// <param name="umbers">Array of integers.</param>
     /// <returns>Last element of the array, or 0 if array is empty.</returns>
     public int GetLastElement(int[] numbers)
     {
-        throw new NotImplementedException();
+        if (numbers.Length == 0)
+        {
+            return 0;
+        }
+        return numbers[numbers.Length - 1];
     }
 
     /// <summary>
@@ -36,16 +45,20 @@ public class ArrayProcessor
     /// <returns>Element at the specified position, or 0 if position is out of range.</returns>
     public int GetElementAtPosition(int[] numbers, int position)
     {
-        throw new NotImplementedException();
+        if (position >= 0 && position < numbers.Length)
+        {
+            return numbers[position];
+        }
+        return 0;
+    }
+        /// <summary>
+        /// Returns the length of the provided array.
+        /// </summary>
+        /// <param name="numbers">Array of integers.</param>
+        /// <returns>Length of the array.</returns>
+        public int GetLength(int[] numbers)
+        {
+        return numbers.Length;
+        }
     }
 
-    /// <summary>
-    /// Returns the length of the provided array.
-    /// </summary>
-    /// <param name="numbers">Array of integers.</param>
-    /// <returns>Length of the array.</returns>
-    public int GetLength(int[] numbers)
-    {
-        throw new NotImplementedException();
-    }
-}
